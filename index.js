615c0253b733187dec0d1064848663db69bfac84
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2025-07-09 16:44:11 +0500

    .

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const log = require("./logger")
+const os = require('os');
 
-log("Salom, Bro!")
+// console.log(os);
+
+const freemem = os.freemem();
+
+console.log(os.freemem());
+
+
+
+// const path = require('node:path');
+
+// console.log(__filename);
+
+// const parseObj = path.parse(__filename);
+
+// console.log(parseObj);
